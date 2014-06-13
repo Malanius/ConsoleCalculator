2f5f1119d4d18992cc4d987b3ae0ad408859191d
Malanius <malanius@seznam.cz>
2014-06-13 19:00:15 +0200

    Initial commit

---
+++ src/consolecalculator/ConsoleCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * This is just example project for learning purposes.
+ */
+
+package consolecalculator;
+
+/**
+ *
+ * @author Malanius
+ */
+public class ConsoleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
