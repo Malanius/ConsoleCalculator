1fe9bb4b53d7525d931b27543cf3c948a0b2cd40
Malanius <malanius@seznam.cz>
2014-06-13 20:54:14 +0200

    Added user input and basic calculation logic.

--- src/consolecalculator/ConsoleCalculator.java
+++ src/consolecalculator/ConsoleCalculator.java
@@ -3,6 +3,7 @@
  */
 
 package consolecalculator;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,30 @@ public class ConsoleCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Variable declaration
+        String input; //For handling user input
+        double numA;
+        double numB;
+        double result;
+        //Setting up scanner
+        Scanner sc = new Scanner(System.in);
+       //Get the first number
+        System.out.print("Please enter the first number: ");
+        input = sc.nextLine();
+        numA = Double.parseDouble(input);
+        //Get the second number
+        System.out.print("Please enter the second number: ");
+        input = sc.nextLine();
+        numB = Double.parseDouble(input);
+        result = numA + numB;
+        System.out.println("Result of " + numA + "+" + numB + " is " + result);
+        result = numA - numB;
+        System.out.println("Result of " + numA + "-" + numB + " is " + result);
+        result = numA * numB;
+        System.out.println("Result of " + numA + "*" + numB + " is " + result);
+        result = numA / numB;
+        System.out.println("Result of " + numA + "/" + numB + " is " + result);
+        
     }
     
 }
