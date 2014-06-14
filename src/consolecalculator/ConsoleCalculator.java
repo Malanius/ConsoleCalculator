84526460273c08362f04f91c17eda3ad76770da6
Malanius <malanius@seznam.cz>
2014-06-14 12:58:50 +0200

    Added loopung until correct input is given.

--- src/consolecalculator/ConsoleCalculator.java
+++ src/consolecalculator/ConsoleCalculator.java
@@ -20,34 +20,51 @@ public class ConsoleCalculator {
         double numA = 0;
         double numB = 0;
         double result = 0;
-        String operation;
+        String operation = "";
+        boolean looped = true;
+        boolean cont = true;
         //Setting up scanner
         Scanner sc = new Scanner(System.in);
         //Get the first number
-        System.out.print("Please enter the first number: ");
-        input = sc.nextLine();
-        //Check if input is number
-        try {
-            numA = Double.parseDouble(input);
-        } catch (NumberFormatException e) {
-            System.err.println(input + " is not a number. Program will terminate.");
-            //System.err.println(e.fillInStackTrace());
-            System.exit(1);
+        while (looped == true) {
+
+            System.out.print("Please enter the first number: ");
+            input = sc.nextLine();
+            //Check if input is number
+            try {
+                numA = Double.parseDouble(input);
+                looped = false;
+            } catch (NumberFormatException e) {
+                System.err.println(input + " is not a number. Please try again.");
+            }
         }
 
         //Get the required operation
-        System.out.print("Please enter the operation (+,-,*,/): ");
-        operation = sc.nextLine();
+        looped = true;
+        while (looped == true) {
+            System.out.print("Please enter the operation (+,-,*,/): ");
+            input = sc.nextLine();
+            if (input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/")){
+                operation = input;
+                break;
+            }
+            else
+            {
+                System.err.println(input + " is invalid operator. Plrase try again.");
+            }
+        }
         //Get the second number
-        System.out.print("Please enter the second number: ");
-        input = sc.nextLine();
-        //Check if input is number
-        try {
-            numB = Double.parseDouble(input);
-        } catch (NumberFormatException e) {
-            System.err.println(input + " is not a number. Program will terminate.");
-            //System.err.println(e.fillInStackTrace());
-            System.exit(1);
+        looped = true;
+        while (looped == true) {
+            System.out.print("Please enter the second number: ");
+            input = sc.nextLine();
+            //Check if input is number
+            try {
+                numB = Double.parseDouble(input);
+                looped = false;
+            } catch (NumberFormatException e) {
+                System.err.println(input + " is not a number. Please try again.");
+            }
         }
         switch (operation) {
             case "+":
@@ -66,7 +83,7 @@ public class ConsoleCalculator {
                 System.err.println("No valid operation defined.");
                 System.exit(2);
         }
-        System.out.println("Result is " + result);
+        System.out.println("Result of " + numA + " " + operation + " " + numB + " = "  + result);
 
     }
 }
