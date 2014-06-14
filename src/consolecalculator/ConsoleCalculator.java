f5cec8ba973cd2b2bde837fb3913b418dbb82129
Malanius <malanius@seznam.cz>
2014-06-14 11:08:27 +0200

    Allowed user to select the operation.

--- src/consolecalculator/ConsoleCalculator.java
+++ src/consolecalculator/ConsoleCalculator.java
@@ -1,8 +1,8 @@
 /*
  * This is just example project for learning purposes.
  */
-
 package consolecalculator;
+
 import java.util.Scanner;
 
 /**
@@ -19,26 +19,34 @@ public class ConsoleCalculator {
         String input; //For handling user input
         double numA;
         double numB;
-        double result;
+        double result = 0;
+        String operation;
         //Setting up scanner
         Scanner sc = new Scanner(System.in);
-       //Get the first number
+        //Get the first number
         System.out.print("Please enter the first number: ");
         input = sc.nextLine();
         numA = Double.parseDouble(input);
+        //Get the required operation
+        System.out.print("Please enter the operation (+,-,*,/): ");
+        operation = sc.nextLine();
         //Get the second number
         System.out.print("Please enter the second number: ");
         input = sc.nextLine();
         numB = Double.parseDouble(input);
-        result = numA + numB;
-        System.out.println("Result of " + numA + "+" + numB + " is " + result);
-        result = numA - numB;
-        System.out.println("Result of " + numA + "-" + numB + " is " + result);
-        result = numA * numB;
-        System.out.println("Result of " + numA + "*" + numB + " is " + result);
-        result = numA / numB;
-        System.out.println("Result of " + numA + "/" + numB + " is " + result);
-        
+        if (operation.equals("+")) {
+            result = numA + numB;
+        } else if (operation.equals("-")) {
+            result = numA - numB;
+        } else if (operation.equals("*")) {
+            result = numA * numB;
+        } else if (operation.equals("/")) {
+            result = numA / numB;
+        } else {
+            System.err.println("No valid operation defined.");
+            System.exit(0);
+        }
+        System.out.println("Result is " + result);
+
     }
-    
 }
